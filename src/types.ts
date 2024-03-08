f33599e532ae2a53b5f6eea5f3e914a779e0285f
Pooya Parsa <pooya@pi0.io>
2024-03-08 15:09:39 +0100

    types: improve context types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
+import type { PlatformProxy } from "wrangler";
+
 declare module "h3" {
   interface H3EventContext {
+    cf: PlatformProxy["cf"];
     cloudflare: {
-      env: {
-        [key: string]: unknown;
-      };
+      request: Request & { cf: PlatformProxy["cf"] };
+      env: PlatformProxy["env"];
+      context: PlatformProxy["ctx"];
     };
   }
 }
