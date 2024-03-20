576ae46077fec6a4611a0511767729603e99a9d8
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-03-20 13:12:27 +0330

    init

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,23 @@
 import type { PlatformProxy } from "wrangler";
 
+type IncomingRequestCfProperties<T> = T extends PlatformProxy<any, infer T>
+  ? T
+  : never;
+
+export interface CfProperties
+  extends IncomingRequestCfProperties<PlatformProxy<any, any>> {}
+
+export interface Env {}
+
+type TypedPlatformProxy = PlatformProxy<Env, CfProperties>;
+
 declare module "h3" {
   interface H3EventContext {
-    cf: PlatformProxy["cf"];
+    cf: TypedPlatformProxy["cf"];
     cloudflare: {
-      request: Request & { cf: PlatformProxy["cf"] };
-      env: PlatformProxy["env"];
-      context: PlatformProxy["ctx"];
+      request: Request & { cf: TypedPlatformProxy["cf"] };
+      env: TypedPlatformProxy["env"];
+      context: TypedPlatformProxy["ctx"];
     };
   }
 }
