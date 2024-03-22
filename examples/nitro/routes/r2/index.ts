55ba741bd3c1e54f04bb0b81d7cac13d80db56b1
Pooya Parsa <pooya@pi0.io>
2024-03-22 15:36:55 +0100

    chore: update html

--- examples/nitro/routes/r2/index.ts
+++ examples/nitro/routes/r2/index.ts
@@ -1,26 +1,36 @@
 export default eventHandler(async (event) => {
-  const bucket = event.context.cloudflare.env.BLOB
-  if (isMethod(event, 'POST')) {
-    console.log('upload image')
-    const form = await readFormData(event)
-    const image = form.get('image') as File
+  const bucket = event.context.cloudflare.env.BLOB;
+  if (isMethod(event, "POST")) {
+    console.log("upload image");
+    const form = await readFormData(event);
+    const image = form.get("image") as File;
 
-    await bucket.put(image.name, image)
+    await bucket.put(image.name, image);
   }
   // List images
   const { objects } = await bucket.list({
     limit: 500,
-    include: ['httpMetadata', 'customMetadata'],
-  })
-  const imgs = objects.map((obj) => {
-    return `<img src="/r2/${obj.key}" alt="${obj.key}" width="300" />`
-  }).join('\n')
+    include: ["httpMetadata", "customMetadata"],
+  });
+  const imgs = objects
+    .map((obj) => {
+      return /* html */ `<img src="/r2/${obj.key}" alt="${obj.key}" width="300" />`;
+    })
+    .join("\n");
 
-  return `
-    <form method="POST" enctype="multipart/form-data">
-      <label>Upload an image: <input type="file" name="image"></label>
-      <button type="submit">Upload</button>
-    </form>
-    ${imgs}
-  `
-})
+  return /* html */ `<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div>
+  <form method="POST" enctype="multipart/form-data">
+    <label for="image">Upload an image:</label>
+    <input type="file" id="image" name="image" accept="image/png,image/jpeg" required>
+    <button type="submit">Upload</button>
+  </form>
+</div>
+<br>
+${imgs}
+  `;
+});
