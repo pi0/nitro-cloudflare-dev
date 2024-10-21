713c45e9431f206b543877bc1f4f1f58e71df664
Sébastien Chopin <seb@nuxt.com>
2024-10-21 14:41:12 +0200

    fix(wrangler): only set environment if defined

--- src/runtime/plugin.dev.ts
+++ src/runtime/plugin.dev.ts
@@ -1,5 +1,5 @@
 import type { NitroAppPlugin } from "nitropack";
-import type { PlatformProxy } from "wrangler";
+import type { GetPlatformProxyOptions, PlatformProxy } from "wrangler";
 // @ts-ignore
 import { useRuntimeConfig, getRequestURL } from "#imports";
 
@@ -71,11 +71,14 @@ async function _getPlatformProxy() {
     };
   } = useRuntimeConfig();
 
-  const proxy = await getPlatformProxy({
+  const proxyOptions: GetPlatformProxyOptions = {
     configPath: runtimeConfig.wrangler.configPath,
     persist: { path: runtimeConfig.wrangler.persistDir },
-    environment: runtimeConfig.wrangler.environment,
-  });
+  };
+  if (runtimeConfig.wrangler.environment) {
+    proxyOptions.environment = runtimeConfig.wrangler.environment;
+  }
+  const proxy = await getPlatformProxy(proxyOptions);
 
   return proxy;
 }
