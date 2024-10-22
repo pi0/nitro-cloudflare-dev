4b0ba851ebeb0eed2b9f0603844a42dab1f85182
Pooya Parsa <pooya@pi0.io>
2024-10-22 19:56:05 +0900

    Update examples/nuxt/nuxt.config.ts

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["nitro-cloudflare-dev"],
-  compatibilityDate: "2024-10-10"
+  compatibilityDate: "2024-10-10",
 });
